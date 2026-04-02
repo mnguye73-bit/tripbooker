4e6a008612bfbb8ca327ae01395e1d5a3bf8ac69
Nathaniel Joseph <njoseph8@charlotte.edu>
2026-04-02 13:16:16 -0400

    more changes to search result

--- src/pages/searchresult/Searchresult.jsx
+++ src/pages/searchresult/Searchresult.jsx
@@ -11,23 +11,30 @@ const Searchresult = () => {
 
 			<input
 				type="text"
-				ClassName="search_box"
+				className="search_box"
 				placeholder="Search a property"
 			/>
 
 			<p className="filters">Filters:</p>
 
 			<div className="card">
-				<img
-					src=""
-					alt=""
-				/>
+				<img src="" alt=""/>
 
 				<div className="card_content">
 					<h3>Capella Hanoi</h3>
 					<ul>
 						<li>Breakfast Included</li>
 						<li>Old Quarter</li>
+					</ul>
+				</div>
+			</div>
+	
+			<div className="card">
+				<img src="" alt=""/>
+				<div className="card_content2">
+					<h3>Hilton Garden In HaNoi</h3>
+					<ul>
+						<li>Pool</li>
 					</ul>
 				</div>
 			</div>
