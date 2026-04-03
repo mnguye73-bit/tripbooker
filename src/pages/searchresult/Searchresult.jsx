e69d412096b1fcef569ca4cb6503d335ce13c876
Nathaniel Joseph <njoseph8@charlotte.edu>
2026-04-03 00:40:23 -0400

    update

--- src/pages/searchresult/Searchresult.jsx
+++ src/pages/searchresult/Searchresult.jsx
@@ -1,8 +1,23 @@
 import './searchresult.css'
+import { FaHome, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 const Searchresult = () => {
 	return(
 		<div className="search_result">
+			<div className="search_container">
+				<input 
+				type="date"
+				className="date_input"
+			/>
+				<input
+					type="text"
+					className="search_input"
+					placeholder="Search a property"
+				/>
+				<button className="search_button">Search</button>
+
+			</div>
+
 			<div className="tabs">
 				<div className="tab active">Hotel</div>
 				<div className="tab">Homes</div>
@@ -16,6 +31,22 @@ const Searchresult = () => {
 			/>
 
 			<p className="filters">Filters:</p>
+			<div className="filter_list">
+  				<div className="filter_item">
+    				<div className="filter_box"></div>
+    				Pool (2)
+  				</div>
+
+  				<div className="filter_item">
+    				<div className="filter_box"></div>
+   					Living Room (1)
+  				</div>
+
+  				<div className="filter_item">
+    				<div className="filter_box"></div>
+    				Backyard (3)
+  				</div>
+			</div>
 
 			<div className="card">
 				<img src="" alt=""/>
@@ -41,6 +72,17 @@ const Searchresult = () => {
 					<p className="rating">6.4 ★</p>
 					<p className="miles">1 mile</p>
 				</div>
+			</div>
+
+			<div className="checkout_footer_bar">
+  				<div className="footer_left">
+    				<FaHome size={28} />
+  				</div>
+
+  				<div className="footer_right">
+    				<FaArrowLeft size={24} />
+    				<FaArrowRight size={24} />
+  				</div>
 			</div>
 		</div>
 	)
