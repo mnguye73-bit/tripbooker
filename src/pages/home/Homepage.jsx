c668cf6c33f4b1a58fe1354ff786d779792cfb0c
mprice97 <mprice97@charlotte.edu>
2026-04-10 14:35:33 -0400

    hover states in home page

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,14 +1,36 @@
 import './homepage.css'
-import { FaMapMarkerAlt, FaCalendarAlt, FaUserPlus, FaBed, FaPlane, FaCar, FaShip, FaUserCircle, FaBars } from 'react-icons/fa' 
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+	FaMapMarkerAlt,
+	FaCalendarAlt,
+	FaUserPlus,
+	FaBed,
+	FaPlane,
+	FaCar,
+	FaShip,
+	FaHome,
+	FaArrowLeft,
+	FaArrowRight
+} from 'react-icons/fa'
 
 const Homepage = () => {
-	return(
-		<div className="home_page">
+	const navigate = useNavigate()
+	const [pageReady, setPageReady] = useState(false)
 
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setPageReady(true)
+		}, 150)
+
+		return () => clearTimeout(timer)
+	}, [])
+
+	return (
+		<div className={`home_page ${pageReady ? 'page_ready' : ''}`}>
 			<h1 className="title">Welcome to TripBooker!</h1>
 
 			<div className="hero">
-
 				<div className="search_bar">
 					<div className="search_item">
 						<FaMapMarkerAlt />
@@ -22,33 +44,68 @@ const Homepage = () => {
 
 					<div className="search_item">
 						<FaUserPlus />
-						<span>Travelers</span> 
+						<span>Travelers</span>
 					</div>
 				</div>
 
 				<div className="options_bar">
-					<div>
+					<div className="option_item">
 						<FaBed />
-						Hotel
+						<span>Hotel</span>
 					</div>
-					<div>
+
+					<div className="option_item">
 						<FaPlane />
-						Flight
+						<span>Flight</span>
 					</div>
-					<div>
+
+					<div className="option_item">
 						<FaCar />
-						Rental
+						<span>Rental</span>
 					</div>
-					<div>	
+
+					<div className="option_item">
 						<FaShip />
-						Cruise
+						<span>Cruise</span>
 					</div>
 				</div>
 
 				<div className="submit_btn">
-					<button>Submit</button>
+					<button type="button" onClick={() => navigate('/searchresult')}>
+						Submit
+					</button>
 				</div>
+			</div>
 
+			<div className="checkout_footer_bar">
+				<button
+					className="footer_nav_btn"
+					aria-label="Home"
+					type="button"
+					onClick={() => navigate('/')}
+				>
+					<FaHome size={20} />
+				</button>
+
+				<div className="footer_right">
+					<button
+						className="footer_nav_btn"
+						aria-label="Previous"
+						type="button"
+						onClick={() => navigate(-1)}
+					>
+						<FaArrowLeft size={18} />
+					</button>
+
+					<button
+						className="footer_nav_btn"
+						aria-label="Next"
+						type="button"
+						onClick={() => navigate('/searchresult')}
+					>
+						<FaArrowRight size={18} />
+					</button>
+				</div>
 			</div>
 		</div>
 	)
