3bd25ab0cd990d575705e711c93c8b11ef4d0b31
mprice97 <mprice97@charlotte.edu>
2026-04-23 23:19:47 -0400

    dark mode fixes

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,16 +1,26 @@
 import "./toggle.css";
 
 function Toggle({ checked, onChange }) {
-  return (
-    <label className="toggle">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-      />
-      <span className="slider"></span>
-    </label>
-  );
+	const handleChange = (e) => {
+		const isChecked = e.target.checked;
+
+		document.body.classList.toggle("dark", isChecked);
+
+		if (onChange) {
+			onChange(e);
+		}
+	};
+
+	return (
+		<label className="toggle">
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={handleChange}
+			/>
+			<span className="slider"></span>
+		</label>
+	);
 }
 
 export default Toggle;
